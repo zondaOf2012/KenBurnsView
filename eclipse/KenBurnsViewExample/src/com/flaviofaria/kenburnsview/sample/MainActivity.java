9e03d36eae68312b6ced9e967cdf8584991232b0
zonda <zhuqiuzhu2013@gmail.com>
2014-03-26 15:55:51 +0800

    提交Eclipse下运行的工程文件

---
+++ eclipse/KenBurnsViewExample/src/com/flaviofaria/kenburnsview/sample/MainActivity.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2014 Flavio Faria
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.flaviofaria.kenburnsview.sample;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.actionbarsherlock.app.SherlockListActivity;
+
+
+public class MainActivity extends SherlockListActivity {
+
+    private static final int POS_SINGLE_IMG = 0;
+    private static final int POS_MULTI_IMG = 1;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.main_options, android.R.layout.simple_list_item_1);
+        setListAdapter(adapter);
+    }
+
+
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        super.onListItemClick(l, v, position, id);
+        switch (position) {
+            case POS_SINGLE_IMG:
+                startActivity(new Intent(this, SingleImageActivity.class));
+                break;
+            case POS_MULTI_IMG:
+                startActivity(new Intent(this, MultiImageActivity.class));
+                break;
+        }
+    }
+}
